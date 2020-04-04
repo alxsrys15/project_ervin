1a082a973b61cf601474b7a8646893f2891ea52f
Julius Allen Reyes <alxsrys15@gmail.com>
2020-04-04 11:13:06 +0800

    added date activated

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 use App\Controller\AppController;
 use Cake\Event\Event;
 use Cake\Utility\Security;
-use Cake\Mailer\Email;
 use Ahc\Jwt\JWT;
 use Cake\Auth\DefaultPasswordHasher;
 /**
@@ -15,66 +14,7 @@ use Cake\Auth\DefaultPasswordHasher;
  * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
  */
 class UsersController extends AppController
-{     
-
-
-
-    /**
-     * Validate token created at time.
-     * @param String $token_created_at
-     * @return Boolean
-     */
-    function __validToken($token_created_at) {
-        $expired = strtotime($token_created_at) + 86400;
-        $time = strtotime("now");
-        if ($time < $expired) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Sends password reset email to user's email address.
-     * @param $id
-     * @return
-     */
-    function __sendForgotPasswordEmail($user_id) {
-          $user = $this->Users->get($user_id);
-          $email = new Email('default');
-          $email->from(['me@example.com' => 'My Site'])
-            ->setViewVars(['user' => $user])
-            ->to('molinaervin@gmail.com')
-            ->subject('About')
-            ->send('My message');
-
-            return true;
-    }
-    
-    function __generatePasswordToken($user) {
-    if (empty($user)) {
-        return null;
-    }
-
-    // Generate a random string 100 chars in length.
-    $token = "";
-    for ($i = 0; $i < 100; $i++) {
-        $d = rand(1, 100000) % 2;
-        $d ? $token .= chr(rand(33,79)) : $token .= chr(rand(80,126));
-    }
-
-    (rand(1, 100000) % 2) ? $token = strrev($token) : $token = $token;
-
-    // Generate hash of random string
-    $hash = Security::hash($token, '', true);;
-    for ($i = 0; $i < 20; $i++) {
-        $hash = Security::hash($hash, '', true);
-    }
-
-    // $user['User']['reset_password_token'] = $hash;
-    // $user['User']['token_created_at']     = date('Y-m-d H:i:s');
-
-    return $user;
-    }
+{
     /**
      * Index method
      *
@@ -110,6 +50,7 @@ class UsersController extends AppController
                 $user = $this->Users->get($user_id);
                 $user->status = "Active";
                 $user->package_id = $this->request->data['package_id'];
+                $user->date_activated = date('Y-m-d H:i:s');
                 if ($this->Users->save($user)) {
                     $this->Flash->success(__('User activated'));
                 }
@@ -270,22 +211,97 @@ class UsersController extends AppController
                     'email' => $this->request->data['Users']['email']
                 ]
             ]);
-            if($query->first()){
-                $user = $query->first()->toArray();
-                $user = $this->Users->get($user['id']);
-                $user = $this->__generatePasswordToken($user);
-                if($this->Users->save($user) && $this->__sendForgotPasswordEmail($user['id'])){
-                    $this->Flash->success('Password reset instructions have been sent to your email address.
-                             You have 24 hours to complete the request.');
-                    return $this->redirect(['action' => 'login']);
-                }else{
-                    $this->Flash->error('Sorry, the email you entered was not found');
-                    return $this->redirect(['action' => 'resetpassword']);
-                }
 
+            if ($query->first()) {// kunin yun first result if exisiting yun email
+                $user = $query->first()->toArray(); // lipat sa var and convert to array form (toArray())
+                $user = $this->Users->get($user['id']); // get yun user data
+                $user = $this->__generatePasswordToken($user); // function mo to generate token
+                if ($this->Users->save($user) && $this->__sendForgotPasswordEmail($user['id'])) {
+                    $this->Flash->success('Password reset instructions have been sent to your email address. You have 24 hours to complete the request.');
+                    return $this->redirect(['action' => 'login']);
+                }
+            } else {
+                $this->Flash->error('Sorry, the username entered was not found');
+                return $this->redirect(['action' => 'resetpassword']);
             }
-        }    
+
+            // if (empty($user)) {
+            //     $this->Session->setflash('Sorry, the username entered was not found.');
+            //     return $this->redirect(['action' => 'resetpassword']);
+            // } else {
+            //     $user = $this->__generatePasswordToken($user);
+            //     if ($this->User->save($user) && $this->__sendForgotPasswordEmail($user['User']['id'])) {
+            //         $this->Session->setflash('Password reset instructions have been sent to your email address.
+            //             You have 24 hours to complete the request.');
+            //          return $this->redirect(['action' => 'login']);
+            //     }
+            // }
+        }
     }
+     function __generatePasswordToken($user) {
+        if (empty($user)) {
+            return null;
+        }
+
+        // Generate a random string 100 chars in length.
+        $token = "";
+        for ($i = 0; $i < 100; $i++) {
+            $d = rand(1, 100000) % 2;
+            $d ? $token .= chr(rand(33,79)) : $token .= chr(rand(80,126));
+        }
+
+        (rand(1, 100000) % 2) ? $token = strrev($token) : $token = $token;
+
+        // Generate hash of random string
+        $hash = Security::hash($token, '', true);;
+        for ($i = 0; $i < 20; $i++) {
+            $hash = Security::hash($hash, '', true);
+        }
+
+        $user['reset_password_token'] = $hash;
+        $user['token_created_at']     = date('Y-m-d H:i:s');
+
+        return $user;
+    }
+
+    /**
+     * Validate token created at time.
+     * @param String $token_created_at
+     * @return Boolean
+     */
+    function __validToken($token_created_at) {
+        $expired = strtotime($token_created_at) + 86400;
+        $time = strtotime("now");
+        if ($time < $expired) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Sends password reset email to user's email address.
+     * @param $id
+     * @return
+     */
+    function __sendForgotPasswordEmail($id = null) {
+        if (!empty($id)) {
+            $this->User->id = $id;
+            $User = $this->User->read();
+
+            $this->Email->to        = ['Users']['email'];
+            $this->Email->subject   = 'Password Reset Request - DO NOT REPLY';
+            $this->Email->replyTo   = 'do-not-reply@example.com';
+            $this->Email->from      = 'Do Not Reply <do-not-reply@example.com>';
+            $this->Email->template  = 'reset_password_request';
+            $this->Email->sendAs    = 'both';
+            $this->set('Users', $User);
+            $this->Email->send();
+
+            return true;
+        }
+        return false;
+    }
+
     /**
      * Notifies user their password has changed.
      * @param $id
@@ -295,7 +311,19 @@ class UsersController extends AppController
         if (!empty($id)) {
             $this->User->id = $id;
             $User = $this->User->read();
+
+            $this->Email->to        = $User['User']['email'];
+            $this->Email->subject   = 'Password Changed - DO NOT REPLY';
+            $this->Email->replyTo   = 'do-not-reply@example.com';
+            $this->Email->from      = 'Do Not Reply <do-not-reply@example.com>';
+            $this->Email->template  = 'password_reset_success';
+            $this->Email->sendAs    = 'both';
+            $this->set('User', $User);
+            $this->Email->send();
+
+            return true;
         }
+        return false;
     }
 
     public function getReferrals () {
@@ -331,19 +359,6 @@ class UsersController extends AppController
                 $this->Flash->error('Incorrect Password');
             }
         }
-
-        $query = $this->Users->find('all', [
-            'condtions' => [
-                'referred_by' => $this->Auth->User('id')
-            ]
-        ]);
-
-        $firstLevelCount = $query->count();
-        $firstLevelIds = [];
-
-        foreach ($query as $referral) {
-            $firstLevelIds[] = $referral->id;
-        }
-
     }
+
 }
