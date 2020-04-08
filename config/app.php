74ff733f1e1c2876de7ea9c1f50bc0a5fc2831e0
Julius Allen Reyes <alxsrys15@gmail.com>
2020-04-08 18:14:17 +0800

    app config

--- config/app.php
+++ config/app.php
@@ -257,7 +257,7 @@ return [
             'driver' => Mysql::class,
             'persistent' => false,
             // 'host' => '107.180.51.1',
-            'host' => 'localhost'
+            'host' => 'localhost',
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
